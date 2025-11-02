06f317b795c3b1eb38e2e91a8e66627cf1013af3
Max Golovanov <max.golovanov+github@gmail.com>
2025-11-01 20:15:56 -0700

    Add threading and URI parsing dependencies
    - Added bshoshany-thread-pool v5.0.0 via vcpkg for C++17 thread pool support
    - Added simple-uri-parser as git submodule for RFC 3986 URI parsing (C++17 compatible)
    - Updated build.cmd to initialize VS Developer Command Prompt
    - Updated CMakeLists.txt to include external/simple-uri-parser headers
    
    Dependencies rationale:
    - BS::thread_pool: Header-only, production-ready, ~500 LOC, better than std::async
    - simple-uri-parser: Header-only, RFC 3986 compliant, C++17 (not C++20 like fix8/uri)

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
 set "PATH=C:\Program Files\CMake\bin;C:\ProgramData\chocolatey\bin;%PATH%"
 cmake -GNinja . --preset windows-default
 pushd out\build\windows-default
